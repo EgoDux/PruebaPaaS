0bf068dc1e1b525ab09d03ed9a5fe968a83e9364
EgoDux <152462078+EgoDux@users.noreply.github.com>
2025-04-19 21:08:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const {CosmosClient}=require('@azure/cosmos');
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+//Configuracin de Cosmos DB
+const cosmosClient=new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
+const database=cosmosClient.database('academic-db');
+const container=database.container('student');
+app.use(express.json());
+
+//ruta para insertar datos
+app.post('/student',async(req,res)=>{
+    try{
+        const{body}=req;
+        const{resource:createdItem}=await container.items.create(body);
+        res.status(201).send(createdItem);
+    }catch(error){
+    res.status(500).send(error);
+    }
+});
+
+//ruta para conultar datos
+app.get('/students',async(req,res)=>{
+    try{        
+        const{resources}=await container.items.readAll().fetchAll();
+        res.status(200).send(resources);
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hola estamos en SENATI desplegando nuestro PaaS 2025');
 });
