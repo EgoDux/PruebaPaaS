fa6edb7cd3202283aed191919b7b40c010594a1e
EgoDux <152462078+EgoDux@users.noreply.github.com>
2025-04-19 19:45:33 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
 const express = require('express');
-const { CosmosClient } = require('@azure/cosmos');
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Configuración de Cosmos DB
-const cosmosClient = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-const database = cosmosClient.database('academic-db');
-const container = database.container('students');
-
-app.use(express.json());
-
-// Ruta para insertar datos
-app.post('/student', async (req, res) => {
-  try {
-    const { body } = req;
-    const { resource: createdItem } = await container.items.create(body);
-    res.status(201).send(createdItem);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Ruta para consultar datos
-app.get('/students', async (req, res) => {
-  try {
-    const { resources } = await container.items.readAll().fetchAll();
-    res.status(200).send(resources);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Ruta raíz
 app.get('/', (req, res) => {
-  res.send('¡Hola desde Azure PaaS con Cosmos DB gratis!');
+    res.send('Hola estamos en SENATI desplegando nuestro PaaS 2025');
 });
 
 app.listen(port, () => {
-  console.log(`Aplicación escuchando en el puerto ${port}`);
+    console.log('Aplicacion escuchando en elpuerto ${port}');
 });
